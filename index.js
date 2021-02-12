610c64da9bdf39bfd265e12a08e2aaf821785e9b
Karl Svartholm <karl@int3software.com>
2021-02-12 16:04:53 +0100

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const fs = require('fs')
+
+const imgPath = process.env.IMG_PATH || './images'
+const baseUrl = process.env.BASE_URL || 'base-url'
+
+const app = express()
+
+app.use((req, res, next) => {
+  res.removeHeader('X-Powered-By')
+  next()
+})
+
+app.set('view engine', 'pug')
+
+app.get(`/${baseUrl}`, (req, res) => {
+  const files = fs.readdirSync(imgPath).sort()
+  res.render('index', { files, baseUrl })
+})
+
+app.use(`/${baseUrl}`, express.static(imgPath))
+
+app.get('*', (req, res) => {
+  res.sendStatus(404)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`snAPP started... port: ${port}`)
+})
