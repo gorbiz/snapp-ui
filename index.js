5a309bf0267da9376ede402cf563ec7aec22813d
Karl Svartholm <gorbiz@gmail.com>
2022-05-20 12:50:38 +0200

    add age indicator to preview

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const fs = require('fs')
-const { exec } = require('child_process')
+const child_process = require('child_process')
 
 const imgPath = process.env.IMG_PATH || '../snaps'
 const title = process.env.TITLE || 'snaps-ui'
@@ -15,20 +15,29 @@ function onlyFolders (path) {
   return list
 }
 
-function getLastFile (path, ext = 'jpg') {
-  const cmd = `(cd ${path} && ls -1 *.${ext}) | tail -n1`
+function exec (cmd) {
   return new Promise((resolve, reject) => {
-    exec(cmd, (error, stdout, stderr) => {
+    child_process.exec(cmd, (error, stdout, stderr) => {
       if (error) return reject(error)
       resolve(stdout.trim())
     })
   })
 }
 
+async function getLastFile (path, ext = 'jpg') {
+  const file = await exec(`(cd ${path} && ls -1 *.${ext}) | tail -n1`)
+  const mtime = fs.statSync(`${path}/${file}`).mtime
+  const age = (new Date() - mtime) / 1000 // seconds
+  return { file, age }
+}
+
 async function makePreview (folder) {
-  const file = await getLastFile(`${imgPath}/${folder}`)
+  const { file, age } = await getLastFile(`${imgPath}/${folder}`)
+  let cls = 'age-above-60m'
+  if (age <= 60 * 60) cls = 'age-below-60m'
+  if (age <= 10 * 60) cls = 'age-below-10m'
   const film = folder.replaceAll(':', '-').toLowerCase() + '.webm'
-  return `<figure>
+  return `<figure class="${cls}">
       <span class="filename">${file}</span>
       <a href="/?folder=${folder}"><img src="/${folder}/${file}" /></a>
       <figcaption><span>${folder}</span><a href="/?folder=${folder}">🖼️ photos</a> <a href='/videos/${film}'>📽️ video</a></figcaption>
